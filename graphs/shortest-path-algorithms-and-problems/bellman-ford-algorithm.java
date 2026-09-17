296db5e2ffd54fa52867fe7b4cbeb1cfd711bad0
Ananya Bajpai <ananya.bajpai729@gmail.com>
2026-09-17 23:45:37 +0530

    Add Bellman-Ford algorithm implementation
    Implement Bellman-Ford algorithm for shortest paths.

---
+++ graphs/shortest-path-algorithms-and-problems/bellman-ford-algorithm.java
@@ -0,0 +1,33 @@
+class Solution {
+    static int[] bellman_ford(int V, ArrayList<ArrayList<Integer>> edges, int S) {
+        int[] dist = new int[V];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[S] = 0;
+
+        for(int i = 0; i < V -1; i++){
+            for(ArrayList<Integer> it: edges){
+                int u = it.get(0);
+                int v = it.get(1);
+                int w = it.get(2);
+
+                if(dist[u] != Integer.MAX_VALUE && dist[u] + w < dist[v]){
+                    dist[v] = dist[u] + w;
+                }
+            }
+        }
+
+        //checking for negative cycle
+        for(ArrayList<Integer> it: edges){
+            int u = it.get(0);
+            int v = it.get(1);
+            int w = it.get(2);
+            if(dist[u] != 1e8 && dist[u] + w < dist[v]){
+                int[] temp = new int[1];
+                temp[0] = -1;
+                return temp;
+            }
+        }
+
+        return dist;
+    }
+}
